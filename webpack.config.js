566e3703567563d3667e095f36e3cd66aece5825
Mehul Patel <mehul8@gmail.com>
2016-08-10 14:35:42 -0700

    use path module in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,30 @@
+var path = require('path')
+
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
   template: __dirname + '/app/index.html',
   filename: 'index.html',
-  inject: 'body'
+  inject: 'body',
 })
+
+var PATHS = {
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'dist'),
+}
 
 module.exports = {
   entry: [
-    './app/index.js'
+    PATHS.app,
   ],
   output: {
-    path: __dirname + '/dist',
-    filename: "index_bundle.js"
+    path: PATHS.build,
+    filename: 'index_bundle.js',
   },
   module: {
     loaders: [
-      {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"},
-      {test: /\.css$/, loader: 'style-loader!css-loader'}
-    ]
+      {test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'},
+      {test: /\.css$/, loader: 'style-loader!css-loader'},
+    ],
   },
-  plugins: [HTMLWebpackPluginConfig]
+  plugins: [HTMLWebpackPluginConfig],
 }
