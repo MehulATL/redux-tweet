99a2c4a38953bef56dedff700654399c2f607390
Mehul Patel <mehul8@gmail.com>
2016-08-10 19:35:07 -0700

    add navigation component and styles

---
+++ app/components/Navigation/Navigation.js
@@ -0,0 +1,40 @@
+import React, { PropTypes } from 'react'
+import { Link } from 'react-router'
+import { container, navContainer, link } from './styles.css'
+
+const NavLinks = ({isAuthed}) => {
+  return isAuthed === true
+    ? <ul>
+        <li><Link className={link} to='/'>{'Home'}</Link></li>
+      </ul>
+    : null
+}
+
+const ActionLinks = ({isAuthed}) => {
+  return isAuthed === true
+    ? <ul>
+        <li>{'New Tweet'}</li>
+        <li><Link className={link} to='/'>{'Logout'}</Link></li>
+      </ul>
+    : <ul>
+        <li><Link className={link} to='/'>{'Home'}</Link></li>
+        <li><Link className={link} to='/auth'>{'Authenticate'}</Link></li>
+      </ul>
+}
+
+const Navigation = ({isAuthed}) => {
+  return (
+    <div className={container}>
+      <nav className={navContainer}>
+        <NavLinks isAuthed={isAuthed} />
+        <ActionLinks isAuthed={isAuthed}/>
+      </nav>
+    </div>
+  )
+}
+
+Navigation.propTypes = NavLinks.propTypes = ActionLinks.propTypes = {
+  isAuthed: PropTypes.bool.isRequired
+}
+
+export default Navigation
