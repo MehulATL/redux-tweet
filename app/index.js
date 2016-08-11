0b4c3da483e224f994db6359ff5a4dbee4bad0f1
Mehul Patel <mehul8@gmail.com>
2016-08-10 22:40:28 -0700

    createStore in app index.js, render react-redux provider

--- app/index.js
+++ app/index.js
@@ -1,4 +1,15 @@
+import React from 'react'
 import ReactDOM from 'react-dom'
 import routes from './config/routes'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import users from 'redux/modules/users'
 
-ReactDOM.render(routes, document.getElementById('app'))
+const store = createStore(users)
+
+ReactDOM.render(
+  <Provider store={store}>
+    {routes}
+  </Provider>,
+  document.getElementById('app')
+)
