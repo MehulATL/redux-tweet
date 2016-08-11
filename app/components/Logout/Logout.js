9079398b73374123653e582e611b3bfa32240aef
Mehul Patel <mehul8@gmail.com>
2016-08-11 00:17:36 -0700

    scaffold logout comp/container

---
+++ app/components/Logout/Logout.js
@@ -0,0 +1,10 @@
+import React, { PropTypes } from 'react'
+import { text } from './styles.css'
+
+const Logout = (props) => {
+  return (
+    <div className={text}>{'You are now logged out.'}</div>
+  )
+}
+
+export default Logout
