6a4ca1e96f01d83c57b1dcd334f340ce76b81245
Mehul Patel <mehul8@gmail.com>
2016-08-10 19:36:11 -0700

    wire up comps and containers

--- app/containers/Main/MainContainer.js
+++ app/containers/Main/MainContainer.js
@@ -1,16 +1,18 @@
 import React from 'react'
+import { Navigation } from 'components'
 import { container, innerContainer } from './styles.css'
 
 const MainContainer = React.createClass({
   render () {
     return (
       <div className={container}>
+        <Navigation isAuthed={true} />
         <div className={innerContainer}>
           {this.props.children}
         </div>
       </div>
     )
-  },
+  }
 })
 
 export default MainContainer
