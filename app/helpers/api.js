eb3ba0a3ce9214762590727e45ef0d4aa1501b8b
Mehul Patel <mehul8@gmail.com>
2016-08-12 00:20:53 -0700

    add api methods to save tweets

---
+++ app/helpers/api.js
@@ -0,0 +1,33 @@
+import { ref } from 'config/constants'
+
+function saveToTweets (tweet) {
+  // We're generating the tweetId by pushing nothing to firebase, and returning the key
+  const tweetId = ref.child('tweets').push().key
+  const tweetPromise = ref.child(`tweets/${tweetId}`)
+    .set({...tweet, tweetId})
+
+  return {
+    tweetId,
+    tweetPromise
+  }
+}
+
+function saveToUsersTweets (tweet, tweetId) {
+  return ref.child(`usersTweets/${tweet.uid}/${tweetId}`)
+    .set({...tweet, tweetId})
+}
+
+function saveLikeCount (tweetId) {
+  return ref.child(`likeCount/${tweetId}`)
+    .set(0)
+}
+
+export function saveTweet (tweet) {
+  const { tweetId, tweetPromise } = saveToTweets(tweet)
+
+  return Promise.all([
+    tweetPromise,
+    saveToUsersTweets(tweet, tweetId),
+    saveLikeCount(tweetId)
+  ]).then(() => ({...tweet, tweetId}))
+}
